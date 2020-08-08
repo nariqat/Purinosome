42ac5425b1c4b84617732cf10bb65cbb17db503d
nariqat <nabeelaariqat@gmail.com>
2020-08-08 16:00:02 -0700

    NEW FILE WITH NEW PARAMS

---
+++ purinosomeModeling-master/add_paths.m
@@ -0,0 +1,13 @@
+% add paths, must be changed for each file system
+% adding the paths allows you to run different functions in all the
+% folders, without being in those folders.
+
+% main path folder for code
+addpath('/Users/nabeelaariqat/Documents/MATLAB/purinosomeModeling-master')
+% numerical code path
+addpath('/Users/nabeelaariqat/Documents/MATLAB/purinosomeModeling-master/numericalcode')
+% analytical code path
+addpath('/Users/nabeelaariqat/Documents/MATLAB/purinosomeModeling-master/analyticcode')
+% figure code path
+addpath('/Users/nabeelaariqat/Documents/MATLAB/purinosomeModeling-master/figurescripts')
+
